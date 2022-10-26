5dab8bbd538bc29517b308bea4568b856886e4fb
raihanhn <raihanhn20@gmail.com>
2022-10-26 17:55:45 +0600

    8th commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Service from './components/Service/Service'
+import Team from './components/Team/Team'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <About/>
       <Service/>
+      <Team/>
     </div>
   )
 }
---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import img0 from '../../assets/img/bra.png'
+
+const Team = () => {
+  return (
+    <div className="tite" id='team'>
+        <h1>Brazil (2022) - National Football Team</h1>
+        <img src={img0} alt="" />
+
+    </div>
+  )
+}
+
+export default Team
